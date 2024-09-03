3ae9bcdce08ef8c42cc0d0c72822c4b83f364d77
lkrra <lkr.rafa@gmail.com>
2024-09-03 02:18:29 +0200

    [New]: isPalindrome returns true when input is a lowercase plindrome

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -4,4 +4,8 @@ describe('palindrome checker', () => {
   test('that isPalindrome is a function', () => {
     expect(typeof isPalindrome).toEqual('function');
   });
+
+  it('should return true if lower case string is a palindrome', () => {
+    expect(isPalindrome('mom')).toBe(true);
+  });
 });
