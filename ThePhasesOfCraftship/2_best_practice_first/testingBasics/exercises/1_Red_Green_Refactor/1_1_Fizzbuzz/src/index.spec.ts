6b0b4aeb8fe062b08bf851ee1d49db4f88b0b3e1
lkrra <lkr.rafa@gmail.com>
2024-09-03 02:09:22 +0200

    Extracted multiple asserts to it.each function

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -9,35 +9,30 @@ describe('fizzbuzz', () => {
     expect(typeof fizzbuzz(1)).toBe('string');
   });
 
-  it('should return fizz for multiples of 3', () => {
-    expect(fizzbuzz(3)).toBe('fizz');
-
-    expect(fizzbuzz(6)).toBe('fizz');
-
-    expect(fizzbuzz(9)).toBe('fizz');
+  it.each([3, 6, 9])('should return fizz when input is divisible by 3', (n) => {
+    expect(fizzbuzz(n)).toBe('fizz');
   });
 
-  it('should return buzz for multiples of 5', () => {
-    expect(fizzbuzz(5)).toBe('buzz');
+  it.each([5, 10, 20])(
+    'should return buzz when input is divisible by 5',
+    (n) => {
+      expect(fizzbuzz(n)).toBe('buzz');
+    }
+  );
 
-    expect(fizzbuzz(10)).toBe('buzz');
+  it.each([15, 30])(
+    'should return fizzbuzz when input is divisible by 3 and 5',
+    (n) => {
+      expect(fizzbuzz(n)).toBe('fizzbuzz');
+    }
+  );
 
-    expect(fizzbuzz(20)).toBe('buzz');
-  });
-
-  it('should return fizzbuzz for multiples of 3 and 5', () => {
-    expect(fizzbuzz(15)).toBe('fizzbuzz');
-
-    expect(fizzbuzz(30)).toBe('fizzbuzz');
-  });
-
-  it('should return string for non-multiples of 3 and 5', () => {
-    expect(fizzbuzz(1)).toBe('1');
-
-    expect(fizzbuzz(2)).toBe('2');
-
-    expect(fizzbuzz(11)).toBe('11');
-  });
+  it.each([1, 2, 11])(
+    'should return string when input is not divisibale by 3 or 5',
+    (n) => {
+      expect(fizzbuzz(n)).toBe(n.toString());
+    }
+  );
 
   it('should throw an error if n is not a number', () => {
     expect(() => fizzbuzz('1' as any)).toThrowError();
