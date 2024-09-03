b7679040cb9fd5a7540051a3a2615d377bf92253
lkrra <lkr.rafa@gmail.com>
2024-09-03 02:27:54 +0200

    [New]: isPalindrome returns false for not palindrome input without spaces and casing

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -16,4 +16,8 @@ describe('palindrome checker', () => {
   it('should return true if string is a palindrome without considering case and spaces', () => {
     expect(isPalindrome('Mom')).toBe(true);
   });
+
+  it('should return false if string is not a palindrome without considering case and spaces', () => {
+    expect(isPalindrome('Hello')).toBe(false);
+  });
 });
