d5b2da3e483adda757693ef0d9b753f848e9acb5
lkrra <lkr.rafa@gmail.com>
2024-09-04 23:12:09 +0200

    [Green]: passwordValidator invalidate input that is not longer than 5

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
@@ -1,7 +1,14 @@
-import { passwordValidator } from './index';
+import { PasswordError, passwordValidator } from './index';
 
 describe('password validator', () => {
   test('that function is defined', () => {
     expect(passwordValidator).toBeDefined();
+  });
+
+  test('that input is not valid if length is less than 5', () => {
+    const result = passwordValidator('asd');
+
+    expect(result.result).toBe(false);
+    expect(result.errors).toContain(PasswordError.LENGTH);
   });
 });
--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.ts
@@ -1,3 +1,28 @@
-export function passwordValidator(password: string): boolean {
-  return true;
+type ValidationResult = {
+  result: boolean;
+  errors: PasswordError[];
+};
+
+export enum PasswordError {
+  LENGTH,
+  NUMBER,
+  UPPERCASE,
+}
+
+export function passwordValidator(password: string): ValidationResult {
+  if (!isLengthValid(password)) {
+    return {
+      result: false,
+      errors: [PasswordError.LENGTH],
+    };
+  }
+
+  return {
+    result: true,
+    errors: [],
+  };
+}
+
+function isLengthValid(password: string): boolean {
+  return password.length >= 5;
 }
