2c7c9c54ea58f50958c2bf5be46ff79ca0ddc1b9
lkrra <lkr.rafa@gmail.com>
2024-09-05 23:58:24 +0200

    [New]: statsCalculator throws error if non-array is passed

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
@@ -6,4 +6,10 @@ describe('stats calculator', () => {
       'Empty array is not allowed'
     );
   });
+
+  it('should throw and error if non-array is passed', () => {
+    expect(() => statsCalculator(1 as any)).toThrowError(
+      'Input should be an array'
+    );
+  });
 });
--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.ts
@@ -1,3 +1,7 @@
 export function statsCalculator(arr: number[]) {
+  if (!Array.isArray(arr)) {
+    throw new Error('Input should be an array');
+  }
+
   throw new Error('Empty array is not allowed');
 }
