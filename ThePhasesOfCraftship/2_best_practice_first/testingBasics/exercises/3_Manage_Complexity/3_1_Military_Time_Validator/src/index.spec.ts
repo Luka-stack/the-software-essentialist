d5254d34106ed1c4a96a438aa5d2af3d949924ba
lkrra <lkr.rafa@gmail.com>
2024-09-11 00:29:14 +0200

    [Tests Update]: added multiple examples for valid military time

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -5,9 +5,16 @@ describe('military time validator', () => {
     expect(validateMilitaryTime('')).toBe(false);
   });
 
-  it('should return true for valid military time', () => {
-    expect(validateMilitaryTime('00:00 - 02:23')).toBe(true);
-  });
+  it.each(['00:00 - 02:23', '00:00 - 23:59', '00:00 - 00:00'])(
+    'should return true for valid military time %s',
+    (timeRange) => {
+      expect(validateMilitaryTime(timeRange)).toBe(true);
+    }
+  );
+
+  //   it('should return true for valid military time', () => {
+  //     expect(validateMilitaryTime('00:00 - 02:23')).toBe(true);
+  //   });
 
   it('should return false for invalid military time', () => {
     expect(validateMilitaryTime('24:00 - 02:23')).toBe(false);
