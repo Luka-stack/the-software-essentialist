66ffefa1a3de540bc54aa71193b708f36205ef3a
lkrra <lkr.rafa@gmail.com>
2024-09-11 00:32:02 +0200

    [Tests Update]: added multiple examples for missformed inputs

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -19,10 +19,6 @@ describe('military time validator', () => {
     }
   );
 
-  it('should return false for invalid military time', () => {
-    expect(validateMilitaryTime('24:00 - 02:23')).toBe(false);
-  });
-
   it('should return false when end time is invalid', () => {
     expect(validateMilitaryTime('00:00 - 02:60')).toBe(false);
   });
@@ -30,6 +26,13 @@ describe('military time validator', () => {
   it('should return false when start time is invalid', () => {
     expect(validateMilitaryTime('25:00 - 02:23')).toBe(false);
   });
+
+  it.each(['00:00 - ', ' - 02:23', '-'])(
+    'should return false for missformed inputs %s',
+    (timeRange) => {
+      expect(validateMilitaryTime(timeRange)).toBe(false);
+    }
+  );
 
   it('should return false when time range is missformed', () => {
     expect(validateMilitaryTime('00:00 - ')).toBe(false);
