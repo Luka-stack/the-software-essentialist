8016ebd5c4877345482ee59b37419af25021c994
lkrra <lkr.rafa@gmail.com>
2024-09-11 00:19:23 +0200

    [New]: validator should return true for valid military time

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -4,4 +4,8 @@ describe('military time validator', () => {
   it('should return false for empty string', () => {
     expect(validateMilitaryTime('')).toBe(false);
   });
+
+  it('should return true for valid military time', () => {
+    expect(validateMilitaryTime('00:00 - 02:23')).toBe(true);
+  });
 });
--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
@@ -1,3 +1,19 @@
 export function validateMilitaryTime(timeRange: string): boolean {
-  return false;
+  const [startTime, endTime] = timeRange.split(' - ');
+  return isValidTime(startTime) && isValidTime(endTime);
+}
+
+function isValidTime(time: string): boolean {
+  const [hour, minute] = time.split(':');
+  return isValidHour(hour) && isValidMinute(minute);
+}
+
+function isValidHour(hour: string): boolean {
+  const hourNumber = parseInt(hour, 10);
+  return hourNumber >= 0 && hourNumber <= 23;
+}
+
+function isValidMinute(minute: string): boolean {
+  const minuteNumber = parseInt(minute, 10);
+  return minuteNumber >= 0 && minuteNumber <= 59;
 }
